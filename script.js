ba3e77b0eb2d80160b191724043a7e884619a161
Azhaghiri <ganadhan6528@gmail.com>
2024-12-29 08:01:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
   { id: 3, name: "Product 3", price: 30 },
   { id: 4, name: "Product 4", price: 40 },
   { id: 5, name: "Product 5", price: 50 },
-  { id: 6, name: "Product 6", price: 60 },
 ];
 
 // Elements
